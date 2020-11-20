53a91d9d9bd11a805ee986574f22bcb227bda0c7
alisghari <alisghari66@gmail.com>
2020-11-20 17:10:56 +0100

    third

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Main from './main';
-
+import App from './App';
 import "bootstrap/dist/css/bootstrap.min.css"
 
 
 ReactDOM.render(
   <React.StrictMode>
-    <Main />
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
